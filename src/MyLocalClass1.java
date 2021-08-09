a17af68072dd587bbf8b85f7ee00618618ffe18e
olcayayata <olcayayata4@gmail.com>
2021-08-08 20:07:46 -0400

    change

---
+++ src/MyLocalClass1.java
@@ -0,0 +1,11 @@
+public class MyLocalClass1 {
+
+    //This is my local class1
+    // i will create a new version
+    //And push this code to remote branch
+
+     public static void main(String[] args) {
+         System.out.println("Learning GitHub is fun!");
+
+    }
+}
---
+++ src/MyLocalClass2.java
@@ -0,0 +1,6 @@
+public class MyLocalClass2 {
+
+    public static void main(String[] args) {
+        System.out.println(" Olcay is learning GitHub");
+    }
+}
